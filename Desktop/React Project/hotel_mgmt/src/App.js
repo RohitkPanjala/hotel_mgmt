5e0ae5d201b33910dfdd6ca1ca7e71e151a73569
RohitkPanjala <rohitgoud710@gmail.com>
2023-09-12 12:22:13 +0100

    project in git

---
+++ Desktop/React Project/hotel_mgmt/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeComponent from './home';
+
+function App() {
+  return (
+    <div className="App">
+  <HomeComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/React Project/hotel_mgmt/src/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeComponent() {
+  return (
+    <div>Welcome to Success</div>
+  )
+}
+
+export default HomeComponent;
